2b68d121f0747a56d6f26a81de9bbb46b57bc672
MatthewPangihutan <143679163+MatthewPangihutan@users.noreply.github.com>
2024-05-06 16:52:21 +0700

    Pair 03 test

--- t10_01.c
+++ t10_01.c
@@ -5,9 +5,11 @@
 #include "./libs/dorm.h"
 #include "./libs/student.h"
 #include "./libs/repository.h"
+#include <stdlib.h>
 
-int main(int _argc, char **_argv)
-{
+
+int main(int _argc, char **_argv){
+    void 
     // codes
     return 0;
 }
